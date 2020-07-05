38f07cf2660a9a3e8db9113dbc9432caf8752de7
Alisa Popova <alisa.popova@gmail.com>
2020-07-05 15:19:37 +0300

    init commit - basic guess number game

---
+++ js/app.js
@@ -0,0 +1,43 @@
+/**
+ *   Define UI vars
+ */
+
+
+const MIN_NUMBER = 0;
+const MAX_NUMBER = 9;
+const RANDOM = Math.floor(Math.random() * MAX_NUMBER) + 1;
+let attempts = 3;
+let gameOver = false;
+
+init();
+
+function init() {
+    document.addEventListener("DOMContentLoaded", loadGame());
+
+}
+
+
+function loadGame() {
+    document.getElementById("minNumber").innerHTML = MIN_NUMBER;
+    document.getElementById("maxNumber").innerHTML = MAX_NUMBER;
+    document.getElementById("submit").addEventListener("click", checkGuess);
+    message = `Guess Number. You have ${attempts} attempts left.`;
+    document.getElementById("message").innerHTML = message;
+}
+
+
+function checkGuess() {
+    if (gameOver) {
+        return;
+    }
+    let guess = document.getElementById("guess");
+    console.log(`checkGuess: guess=${guess.value}`);
+    const win = guess.value === RANDOM;
+    attempts--;
+    document.getElementById("message").innerHTML = win ? `Correct. You won` : `Your Guess is Wrong. Try Again. ${attempts} attempts left.`;
+    if (attempts === 0 || win) {
+        gameOver = true;
+        guess.disabled = true;
+    }
+
+}
